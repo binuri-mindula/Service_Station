a79a8efbe78c864e87b1204f378047d3d07e3db2
Binuri Mindula <binuriwagarachchi@gmail.com>
2025-06-24 14:25:23 +0530

    Add project files.

---
+++ Test app/Controllers/ServicesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_app.Models;
+
+
+namespace Test_app.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            var service = new VehicalService();
+            service.Id = 1;
+            service.Title = "Oil change";
+            service.Description = "Change the oil in your vehicle";
+            service.Cost = 32.23m;
+            service.IsDeleted = false;
+
+            return View(service);
+        }
+    }
+}
---
+++ Test app/Models/VehicalService.cs
@@ -0,0 +1,12 @@
+﻿namespace Test_app.Models
+{
+    public class VehicalService
+    {
+        //POCO - Plain Old Class Object
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
